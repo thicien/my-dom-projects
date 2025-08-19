2aa55448809a0d615c94e52377ed9f28c89c531c
mugisha thicien <mugishathicien04@gmail.com>
2025-08-19 18:11:22 +0200

    all commited

---
+++ index.js
@@ -0,0 +1,28 @@
+// function exchangeArrays(arr1, arr2) {
+
+//   let rev1 = [...arr1].reverse();
+//   let rev2 = [...arr2].reverse();
+//   arr1.length = 0;
+//   arr1.push(...rev2);
+//   arr2.length = 0;
+//   arr2.push(...rev1);
+// }
+// let myArray = ['a', 'b', 'c'];
+// let otherArray = [1, 2, 3];
+
+// exchangeArrays(myArray, otherArray);
+
+// console.log(myArray);
+// console.log(otherArray);  
+function primeFactors(a, b) {
+    for(let i = 0; i < b; i++){
+        if(b % i === 0) {
+            while(b % i === 0) b /= i;
+            if(b % i !== 0) return false; 
+        }
+    }
+    return true;
+}
+console.log(primeFactors(30, 12));
+console.log(primeFactors(15, 12));
+console.log(primeFactors(45, 15));
