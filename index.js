55a076d52bc55bbf4906e40a93f31c877d9f017e
mugisha thicien <mugishathicien04@gmail.com>
2025-08-20 16:38:02 +0200

    commented

--- index.js
+++ index.js
@@ -13,16 +13,33 @@
 // exchangeArrays(myArray, otherArray);
 
 // console.log(myArray);
-// console.log(otherArray);  
-function primeFactors(a, b) {
-    for(let i = 0; i < b; i++){
-        if(b % i === 0) {
-            while(b % i === 0) b /= i;
-            if(b % i !== 0) return false; 
+// console.log(otherArray);
+// function primeFactors(a, b) {
+//     for(let i = 0; i < b; i++){
+//         if(b % i === 0) {
+//             while(b % i === 0) b /= i;
+//             if(b % i !== 0) return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(primeFactors(30, 12));
+// console.log(primeFactors(15, 12));
+// console.log(primeFactors(45, 15));
+function allPrimeFactorsDivisible(a, b) {
+    let num = b;
+    let i = 2;
+    while (i <= num) {
+        if (num % i === 0) {
+            if (a % i !== 0) return false; 
+            while (num % i === 0) num /= i; 
+        } else {
+            i++;
         }
     }
     return true;
 }
-console.log(primeFactors(30, 12));
-console.log(primeFactors(15, 12));
-console.log(primeFactors(45, 15));
+
+console.log(allPrimeFactorsDivisible(30, 12)); 
+console.log(allPrimeFactorsDivisible(15, 12)); 
+console.log(allPrimeFactorsDivisible(45, 15)); 
